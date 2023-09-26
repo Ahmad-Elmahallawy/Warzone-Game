f750adec2df1697cf3d0d017be147e0b25f9446d
Mahanaim Rubin Yo <mahanaimyo@gmail.com>
2023-09-26 13:28:46 -0400

    added all methods of cpp file
    FInished for A1

--- Orders.cpp
+++ Orders.cpp
@@ -25,11 +25,37 @@ Deploy::Deploy(Deploy& deploy)
 	this->action = deploy.action;
 }
 
+bool Deploy :: validate()
+{
+	cout << "deploy class is valid"<< endl;
+}
+
+void Deploy::execute()
+{
+	if (this->validate() == true)
+	{
+		cout << "executing Order deploy" << endl;
+	}
+}
+
 //for advance subclass
 Advance::Advance() {}
 Advance::Advance(Advance& advance) 
 {
 	this->action = advance.action;
+}
+
+bool Advance::validate()
+{
+	cout << "Advance class is valid" << endl;
+}
+
+void Advance::execute()
+{
+	if (this->validate() == true)
+	{
+		cout << "executing Order Advance" << endl;
+	}
 }
 
 //for bomb subclass
@@ -39,6 +65,19 @@ Bomb::Bomb(Bomb& bomb)
 	this->action = bomb.action;
 }
 
+bool Bomb::validate()
+{
+	cout << "Bomb class is valid" << endl;
+}
+
+void Bomb::execute()
+{
+	if (this->validate() == true)
+	{
+		cout << "executing Bomb deploy" << endl;
+	}
+}
+
 //for blockade subclass
 Blockade::Blockade() {}
 Blockade::Blockade(Blockade& blockade) 
@@ -46,11 +85,37 @@ Blockade::Blockade(Blockade& blockade)
 	this->action = blockade.action;
 }
 
+bool Blockade::validate()
+{
+	cout << "Blockade class is valid" << endl;
+}
+
+void Blockade::execute()
+{
+	if (this->validate() == true)
+	{
+		cout << "executing Order Blockade" << endl;
+	}
+}
+
 //for airlift subclass
 Airlift::Airlift() {}
 Airlift::Airlift(Airlift& airlift)
 {
 	this->action = airlift.action;
+}
+
+bool Airlift::validate()
+{
+	cout << "Airlift class is valid" << endl;
+}
+
+void Airlift::execute()
+{
+	if (this->validate() == true)
+	{
+		cout << "executing Order Airlift" << endl;
+	}
 }
 
 
@@ -61,5 +126,48 @@ Negotiate::Negotiate(Negotiate& negotiate)
 	this->action = negotiate.action;
 }
 
+bool Negotiate::validate()
+{
+	cout << "Negotiate class is valid" << endl;
+}
 
+void Negotiate::execute()
+{
+	if (this->validate() == true)
+	{
+		cout << "executing Order Negotiate" << endl;
+	}
+}
+
+
+//orders list class
+
+OrdersList::OrdersList() {}
+OrdersList::OrdersList(OrdersList& ordersList)
+{
+	this->ordersList = ordersList.ordersList;
+}
+
+//add an order to OrdesList
+void OrdersList::addOrder(Order* order)
+{
+	ordersList.push(order);
+}
+
+// get next order on the list
+Order* OrdersList::getNextOrder()
+{
+	// if list not empty, pop and return next order
+	if (!ordersList.empty())
+	{
+		Order* nextOrderPtr = ordersList.front();
+		ordersList.pop();
+		return nextOrderPtr;
+	}
+	else
+	{   // if list is empty, print a message
+		std::cout << " nothing to get: Order list is empty !";
+		return nullptr;
+	}
+}
 
