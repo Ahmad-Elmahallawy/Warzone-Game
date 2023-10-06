788d066d66afe092c4c4957edb17d0ff7c29ef00
Mahanaim Rubin Yo <mahanaimyo@gmail.com>
2023-10-06 00:54:25 -0400

    game engine driver instructions

--- GameEngineDriver.cpp
+++ GameEngineDriver.cpp
@@ -9,40 +9,65 @@
 
 
 void start()
-{}
+{
+    std::cout << "started game\n"
+    <<std::endl;
+    std::cout << "options: loadMap" <<std::endl;
+}
 void loadMap()
-{}
+{
+    std::cout <<"Map loaded\n"
+    <<std::endl;
+    std::cout<< "options: 'loadMap', 'validateMap'" << std::endl;
+
+}
 void validateMap()
 {
-
+    std::cout <<"validated map\n"
+    <<std::endl;
+    std::cout <<"options: 'addPlayer'"<<std::endl;
 }
 void addPlayer()
 {
-
+    std::cout <<"Added Player\n"
+              <<std::endl;
+    std::cout <<"options: 'addPlayer', 'assignCountries'"<<std::endl;
 }
 void assignCountries()
 {
-
+    std::cout <<"Assigned countries\n"
+              <<std::endl;
+    std::cout <<"options: 'issueOrder'"<<std::endl;
 }
 void issueOrder()
 {
-
+    std::cout <<"issued Order\n"
+              <<std::endl;
+    std::cout <<"options: 'issueOrder', 'endIssueOrders'"<<std::endl;
 }
 void endIssueOrders()
 {
-
+    std::cout <<"Ended issuing Orders\n"
+              <<std::endl;
+    std::cout <<"options: 'executeOrder', 'endExecuteOrders', 'win' "<<std::endl;
 }
 void executeOrder()
 {
-
+    std::cout <<"executed Order\n"
+              <<std::endl;
+    std::cout <<"options: 'executeOrder', 'endExecuteOrders', 'win'  '"<<std::endl;
 }
 void endExecuteOrders()
 {
-
+    std::cout <<"DOne executing Orders\n"
+              <<std::endl;
+    std::cout <<"options: 'issueOrder' "<<std::endl;
 }
 void win()
 {
-
+    std::cout <<"congrats you won!\n"
+              <<std::endl;
+    std::cout <<"options: 'play', end' "<<std::endl;
 }
 void end()
 {
