3eba945a164166bea7e9632950085e71202a5f1f
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2023-10-05 22:58:29 -0400

    everything working so far

---
+++ cmake-build-debug/CMakeFiles/Project_1.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_1.dir/Cards/Cards.cpp.obj"
+  "CMakeFiles/Project_1.dir/Cards/Cards.cpp.obj.d"
+  "CMakeFiles/Project_1.dir/Cards/CardsDriver.cpp.obj"
+  "CMakeFiles/Project_1.dir/Cards/CardsDriver.cpp.obj.d"
+  "CMakeFiles/Project_1.dir/GameEngine.cpp.obj"
+  "CMakeFiles/Project_1.dir/GameEngine.cpp.obj.d"
+  "CMakeFiles/Project_1.dir/Map/Map.cpp.obj"
+  "CMakeFiles/Project_1.dir/Map/Map.cpp.obj.d"
+  "CMakeFiles/Project_1.dir/Map/MapDriver.cpp.obj"
+  "CMakeFiles/Project_1.dir/Map/MapDriver.cpp.obj.d"
+  "CMakeFiles/Project_1.dir/Orders/Orders.cpp.obj"
+  "CMakeFiles/Project_1.dir/Orders/Orders.cpp.obj.d"
+  "CMakeFiles/Project_1.dir/Orders/OrdersDriver.cpp.obj"
+  "CMakeFiles/Project_1.dir/Orders/OrdersDriver.cpp.obj.d"
+  "CMakeFiles/Project_1.dir/Players/Player.cpp.obj"
+  "CMakeFiles/Project_1.dir/Players/Player.cpp.obj.d"
+  "CMakeFiles/Project_1.dir/Players/PlayerDriver.cpp.obj"
+  "CMakeFiles/Project_1.dir/Players/PlayerDriver.cpp.obj.d"
+  "Project_1.exe"
+  "Project_1.exe.manifest"
+  "Project_1.pdb"
+  "libProject_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
