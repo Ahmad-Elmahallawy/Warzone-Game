6ecc2df92810081f2a93dbcc4d8e0d54e1cb36af
Omar Shehata <o.i.m.ab.s.a@gmail.com>
2023-11-27 17:48:23 -0500

    More gameengines

--- GameEngineDriver.cpp
+++ GameEngineDriver.cpp
@@ -7,8 +7,20 @@
 // function that runs the game engine
 void testStartupPhase() {
     GameEngine gameEngine;
-    gameEngine.startupPhase();
-    gameEngine.mainGameLoop();
+    GameEngine gameEngine2;
+    GameEngine gameEngine3;
+    GameEngine gameEngine4;
+    GameEngine gameEngine5;
+    std::vector<GameEngine> gameEngines;
+    gameEngines.push_back(gameEngine);
+    gameEngines.push_back(gameEngine2);
+    gameEngines.push_back(gameEngine3);
+    gameEngines.push_back(gameEngine4);
+    gameEngines.push_back(gameEngine5);
+    for(int i = 0;i<gameEngines.size();i++){
+        gameEngines[i].startupPhase();
+        gameEngines[i].mainGameLoop();
+    }
 }
 
 
